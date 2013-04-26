dcab350933a3e52173881e4593f277e066a0716b
Rhiath <nlak@gmx.de>
2013-04-26 22:15:08 +0200

    - created java-lib project - added Generic class for translation of Collection into an Array

---
+++ java-lib/src/main/java/de/raytec/java/lib/Generic.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.raytec.java.lib;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ *
+ * @author raymoon
+ */
+public abstract class Generic {
+    public static <T> T[] toArray(Collection<T> values, Class<? extends T> theClass){
+        T[] retValue = null;
+        
+        if( values != null ){
+            retValue = (T[]) Array.newInstance(theClass, values.size());
+           
+            int position = 0;
+            for ( T value : values ){
+                retValue[position++] = value;
+            }
+        }
+        
+        return retValue;
+    }
+}
